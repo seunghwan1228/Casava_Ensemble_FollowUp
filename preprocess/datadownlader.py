dd8d814cda49acbced5439dfde392f0e35c7e70b
Han <50775491+seunghwan1228@users.noreply.github.com>
2021-03-06 17:20:59 +0900

    Annot: Working Resnext Model

---
+++ preprocess/datadownlader.py
@@ -0,0 +1,5 @@
+import numpy as np
+import tensorflow as tf
+import tensorflow_datasets as tfds
+import os
+
---
+++ utils/config_manager.py
@@ -0,0 +1,40 @@
+from pathlib import Path
+import numpy as np
+import ruamel.yaml
+import os
+
+
+class ConfigManager:
+    def __init__(self, config_path):
+        self.config_path = Path(config_path)
+        self.yaml_loader = ruamel.yaml.YAML()
+
+    def _read_config(self, config_file):
+        with open(config_file, 'rb') as cfg:
+            config_dict = self.yaml_loader.load(cfg)
+        return config_dict
+
+    def _load_multiple_config(self):
+        config_file_list = [self.config_path / Path(cfg_file)  for cfg_file in os.listdir(self.config_path) if cfg_file.endswith('.yaml')]
+        return config_file_list
+
+    def _merge_config_dict(self, list_config_dict):
+        config_dict = {}
+        for cfg_file in list_config_dict:
+            loaded_config = self._read_config(cfg_file)
+            config_dict.update(loaded_config)
+        return config_dict
+
+    def get_config(self):
+        config_list = self._load_multiple_config()
+        configs = self._merge_config_dict(config_list)
+        return configs
+
+
+
+
+if __name__ == "__main__":
+    tmp_config = ConfigManager('config/resnext')
+    tmp_list = tmp_config._load_multiple_config()
+    tmp_full_loaded = tmp_config._merge_config_dict(tmp_list)
+    print(tmp_full_loaded)
