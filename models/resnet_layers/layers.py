8a98d0dabe33c7c5588a8ed790a6aa974a9e8231
seunghwan <seunghwan1228@gmail.com>
2021-03-18 17:07:36 +0900

    Required: Test required

--- models/resnet_layers/layers.py
+++ models/resnet_layers/layers.py
@@ -1,4 +1,5 @@
 import tensorflow as tf
+from tensorflow.python.keras.layers.convolutional import Conv
 from tensorflow.python.ops.gen_array_ops import identity
 
 
@@ -39,6 +40,7 @@ class ResNetBlock(tf.keras.layers.Layer):
         conv1_output = self.conv_1(inputs)
         conv2_output = self.conv_2(conv1_output)
         return conv2_output + residual
+
     
 class ResNetBlockExpand(tf.keras.layers.Layer):
     """[summary]
@@ -64,7 +66,54 @@ class ResNetBlockExpand(tf.keras.layers.Layer):
         
         conv_expanded = self.conv_expand(residual)
         return conv_expanded + conv2_output
+
+
+class ResNeXtBlock(tf.keras.layers.Layer):
+    """[summary]
+        Operating Residual Connection Block
+        
+        Conv1 - Norm - Activation - Conv2 - Norm - Activation - Conv3 - Norm - Activation
+        Input                                                                               - Connection(+ Operation)
+        -------------------------------------------------------------------------------------------------------------
+        
+        Pair as "Deep Residual Learning for Image Recognition" Fig.3 < Straight Line >
+    """
+    def __init__(self, conv_1_2_filters, conv_3_filters, groups, **kwargs):
+        super(ResNeXtBlock, self).__init__(**kwargs)
+        self.conv_1 = ConvOps(filters=conv_1_2_filters, kernel_size=(1,1), strides=(1,1), padding='same', groups=1)
+        self.conv_2 = ConvOps(filters=conv_1_2_filters, kernel_size=(3,3), strides=(1,1), padding='same', groups=groups)
+        self.conv_3 = ConvOps(filters=conv_3_filters, kernel_size=(3,3), strides=(1,1), padding='same', groups=1)
     
+    def call(self, inputs):
+        residual = inputs
+        x = self.conv_1(inputs)
+        x = self.conv_2(x)
+        x = self.conv_3(x)
+        return x + residual
+
+
+class ResNeXtBlockExpand(tf.keras.layers.Layer):
+    """[summary]
+        Operating Residual Connection Block
+        
+        Conv1 - Norm - Activation - Conv2 - Norm - Activation - Conv3 - Norm - Activation
+        Input - Conv_Expand(1x1)                                                            - Connection(+ Operation)
+        -------------------------------------------------------------------------------------------------------------
+    """
+    def __init__(self, conv_1_2_filters, conv_3_filters, strides, groups, **kwargs):
+        super(ResNeXtBlockExpand, self).__init__(**kwargs)
+        self.conv_1 = ConvOps(filters=conv_1_2_filters, kernel_size=(1,1), strides=(strides,strides), padding='same', groups=1)
+        self.conv_2 = ConvOps(filters=conv_1_2_filters, kernel_size=(3,3), strides=(1,1), padding='same', groups=groups)
+        self.conv_3 = ConvOps(filters=conv_3_filters, kernel_size=(3,3), strides=(1,1), padding='same', groups=1)
+        self.conv_expand = tf.keras.layers.Conv2D(filters=conv_3_filters, kernel_size=(1,1), strides=(strides, strides), padding='same')
+    
+    def call(self, inputs):
+        residual = inputs
+        x = self.conv_1(inputs)
+        x = self.conv_2(x)
+        x = self.conv_3(x)
+        expander = self.conv_expand(residual)
+        return x + expander
 
         
     
@@ -78,7 +127,7 @@ if __name__ == "__main__":
     
     test_input = tf.random.uniform(shape=(1, 32, 32, 3))
     
-    tmp_convop = ConvOps(32, 3, 1, 'same', 1)
+    tmp_convop = ConvOps(32, 3, 2, 'same', 1)
     tmp_conv_result = experiment(test_input, tmp_convop)
     
     tmp_residual_plain = ResNetBlock(32, 3, 1, 1, 'same', 1)
@@ -92,5 +141,13 @@ if __name__ == "__main__":
     
     tmp_residual_expand = ResNetBlockExpand(64, 3, 2, 1, 'same', 4)
     tmp_residual_expand_result = experiment(tmp_conv_result, tmp_residual_expand)
+    
+    tmp_resnext_expand = ResNeXtBlockExpand(128, 256, 1, 32)
+    tmp_resnext_expand_result = experiment(tmp_conv_result, tmp_resnext_expand)
+    
+    tmp_resnext_plain = ResNeXtBlock(128, 256, 32)
+    tmp_resnext_plain_result = experiment(tmp_resnext_expand_result, tmp_resnext_plain)
+    
+    
     
     
---
+++ models/resnet_layers/resnext.py
@@ -0,0 +1,102 @@
+import tensorflow as tf
+from tensorflow.python.keras.layers.convolutional import Conv
+from models.resnet_layers.layers import ConvOps, ResNeXtBlockExpand, ResNetBlock, ResNetBlockExpand, ResNeXtBlock, ResNetBlockExpand
+
+
+
+class ResNeXt50(tf.keras.Model):
+    def __init__(self, config):
+        super(ResNeXt50, self).__init__()
+        self.config = config
+        self.base_conv = ConvOps(filters=self.config['base_conv_filters'],
+                                 kernel_size=(self.config['base_conv_kernel'], self.config['base_conv_kernel']),
+                                 strides=(self.config['base_conv_strides'], self.config['base_conv_strides']),
+                                 padding='same',
+                                 groups=1)
+        
+        self.base_pool = tf.keras.layers.MaxPool2D(pool_size=(self.config['maxpool_size'], self.config['maxpool_size']),
+                                                   strides=self.config['maxpool_stride'])
+        
+        self.block_1_exp = ResNeXtBlockExpand(conv_1_2_filters=self.config['conv1_1_2_filters'], 
+                                              conv_3_filters=self.config['conv1_3_filters'],
+                                              strides=1,
+                                              groups=self.config['cardinality'])
+        
+        self.block_1 = [ResNeXtBlock(conv_1_2_filters=self.config['conv1_1_2_filters'], 
+                                     conv_3_filters=self.config['conv1_3_filters'],
+                                     groups=self.config['cardinality']) for _ in range(self.config['num_conv1_layers'] - 1)]
+        
+        
+        self.block_2_exp = ResNeXtBlockExpand(conv_1_2_filters=self.config['conv2_1_2_filters'],
+                                              conv_3_filters=self.config['conv2_3_filters'],
+                                              strides=2,
+                                              groups=self.config['cardinality'])
+        self.block_2 = [ResNeXtBlock(conv_1_2_filters=self.config['conv2_1_2_filters'],
+                                     conv_3_filters=self.config['conv2_3_filters'],
+                                     groups=self.config['cardinality']) for _ in range(self.config['num_conv2_layers'] - 1)]
+        
+        
+        self.block_3_exp = ResNeXtBlockExpand(conv_1_2_filters=self.config['conv3_1_2_filters'],
+                                              conv_3_filters=self.config['conv3_3_filters'],
+                                              strides=2,
+                                              groups=self.config['cardinality'])
+        self.block_3 = [ResNeXtBlock(conv_1_2_filters=self.config['conv3_1_2_filters'],
+                                     conv_3_filters=self.config['conv3_3_filters'],
+                                     groups=self.config['cardinality']) for _ in range(self.config['num_conv3_layers'] - 1)]
+        
+        self.block_4_exp = ResNeXtBlockExpand(conv_1_2_filters=self.config['conv4_1_2_filters'],
+                                              conv_3_filters=self.config['conv4_3_filters'],
+                                              strides=2,
+                                              groups=self.config['cardinality'])
+        self.block_4 = [ResNeXtBlock(conv_1_2_filters=self.config['conv4_1_2_filters'],
+                                     conv_3_filters=self.config['conv4_3_filters'],
+                                     groups=self.config['cardinality']) for _ in range(self.config['num_conv4_layers'] - 1)]
+        
+        
+        self.global_pool = tf.keras.layers.GlobalAveragePooling2D()
+        self.output_layer = tf.keras.layers.Dense(self.config['num_classes'])
+        
+    def call(self, inputs):
+        base_conv_result = self.base_conv(inputs)
+        base_conv_pool = self.base_pool(base_conv_result)
+        
+        block_1_expanditure = self.block_1_exp(base_conv_pool)
+
+        block_1_x = block_1_expanditure
+        for block_1_layer in self.block_1:
+            block_1_x = block_1_layer(block_1_x)
+        
+        block_2_expanditure = self.block_2_exp(block_1_x)
+        block_2_x = block_2_expanditure
+        for block_2_layer in self.block_2:
+            block_2_x = block_2_layer(block_2_x)
+            
+        
+        block_3_expanditure = self.block_3_exp(block_2_x)
+        block_3_x = block_3_expanditure
+        for block_3_layer in self.block_3:
+            block_3_x = block_3_layer(block_3_x)
+    
+    
+        block_4_expanditure = self.block_4_exp(block_3_x)
+        block_4_x = block_4_expanditure
+        for block_4_layer in self.block_4:
+            block_4_x = block_4_layer(block_4_x)
+        
+        global_pool_result = self.global_pool(block_4_x)
+        
+        output_ =  self.output_layer(global_pool_result)
+        return output_
+
+if __name__ == "__main__":
+    from utils.config_manager import ConfigManager
+    
+    config_manager = ConfigManager('config/resnext')
+    
+    config = config_manager.get_config()
+    
+    config_manager.print_config(config)
+    tmp_input = tf.random.uniform(shape=(2, 224, 224, 3))
+    tmp_model = ResNeXt50(config=config)
+    tmp_output = tmp_model(tmp_input)
+    
